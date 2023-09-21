444736cfa7f8015c8fb4b4c86ae3a7e0556fc798
aiartstyle50 <112304615+aiartstyle50@users.noreply.github.com>
2023-09-21 11:45:45 -0400

    Update cerousel.tsx

--- pfft/src/components/cerousel.tsx
+++ pfft/src/components/cerousel.tsx
@@ -32,7 +32,7 @@ const HeadlineCarousel: React.FC<Props> = () => {
     <div className="">
       <ReactTicker
         data={myHeadlines?.map((item) => item)}
-        speed={(myHeadlines.length || 0) + 12}
+        speed={(myHeadlines.length || 0) + 42}
         keyName="_id"
       />
     </div>
