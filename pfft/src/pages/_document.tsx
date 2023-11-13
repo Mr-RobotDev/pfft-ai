bac4284ea4abe407083823facc8319dd8b84e734
Berneleys Jimenez <104395511+jberneleys@users.noreply.github.com>
2023-11-13 16:53:46 +0500

    Added Tag Manager Snippit

--- pfft/src/pages/_document.tsx
+++ pfft/src/pages/_document.tsx
@@ -1,15 +1,24 @@
-'use client'
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+"use client";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
-import { AppConfig } from '@/utils/AppConfig';
-
+import { AppConfig } from "@/utils/AppConfig";
+import Script from 'next/script';
 // Need to create a custom _document because i18n support is not compatible with `next export`.
 class MyDocument extends Document {
   // eslint-disable-next-line class-methods-use-this
   render() {
     return (
       <Html lang={AppConfig.locale}>
-        <Head />
+        <Head>
+<Script async strategy="afterInteractive"  src="https://www.googletagmanager.com/gtag/js?id=G-DZJ4G6H5LK"></Script>
+<Script strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-DZJ4G6H5LK');`
+}}>
+</Script>
+        </Head>
         <body>
           <Main />
           <NextScript />
