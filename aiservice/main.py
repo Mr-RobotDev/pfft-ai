32ea08d205ce1dae2908a6ac1d1c8bdd1ef266ec
aiartstyle50 <112304615+aiartstyle50@users.noreply.github.com>
2023-12-09 19:09:52 -0500

    Update main.py
    update stop sequence

--- aiservice/main.py
+++ aiservice/main.py
@@ -68,19 +68,19 @@ def process_opinion(opinion: str, processing_count: int) -> str:
         prompt=prompt,
         temperature=0.8,
         max_tokens=60,
-        stop=["###"]
+        stop=["##"]
     )
 
     processed_opinion = response.choices[0].text.strip()
     return processed_opinion
     
 def check_and_retry(prompt: str, engine="davinci:ft-ai100-2023-06-03-18-54-09") -> str:
-    output = generate_text(prompt, engine=engine, stop="###")
+    output = generate_text(prompt, engine=engine, stop="##")
     plagiarism_results = check_plagiarism([output], spreadsheet_data)
     if not plagiarism_results:
         return output
     else:
-        output = generate_text(prompt, engine=engine, stop="###")
+        output = generate_text(prompt, engine=engine, stop="##")
         plagiarism_results = check_plagiarism([output], spreadsheet_data)
         if not plagiarism_results:
             return output
@@ -122,7 +122,7 @@ def generate_article():
         new_prompt = f"The following is a professional satire writing tool created by the greatest satirical headline writer of all time. It hides an idea or opinion in a satirical news headline by passing this idea or opinion through one or more humor filters such as irony, exaggeration, wordplay, reversal, shock, hyperbole, incongruity, meta humor, benign violation, madcap, unexpected endings, character, reference, brevity, parody, rhythm, analogy, and/or misplaced focus and outputs a hilarious satirical headline. Begin: {processed_opinion} -> {headline} ###Add Article:"
     
         # Generate the article
-        new_result = generate_text(new_prompt, engine="davinci:ft-ai100-2023-05-22-06-41-36", max_tokens=400, stop=["!Article Complete","!E","###"])
+        new_result = generate_text(new_prompt, engine="davinci:ft-ai100-2023-05-22-06-41-36", max_tokens=400, stop=["!Article Complete","!E","###","##"])
 
         flagged, moderation_output = moderate_content(new_result)
     
@@ -131,7 +131,7 @@ def generate_article():
             print("jh" , new_result)
         else:
             # Rerun the article generation if it's flagged
-            new_result = generate_text(new_prompt, engine="davinci:ft-ai100-2023-05-22-06-41-36", max_tokens=400, stop=["!Article Complete","!E","###"])
+            new_result = generate_text(new_prompt, engine="davinci:ft-ai100-2023-05-22-06-41-36", max_tokens=400, stop=["!Article Complete","!E","###","##"])
             flagged, moderation_output = moderate_content(new_result)
             if not flagged:
                 print(f"\nArticle Generated")
