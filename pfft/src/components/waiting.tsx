4c8eae9398fbfda934d405acb38d899b50d56545
aiartstyle50 <112304615+aiartstyle50@users.noreply.github.com>
2023-12-29 08:41:56 -0500

    revert to original waiting screen

--- pfft/src/components/waiting.tsx
+++ pfft/src/components/waiting.tsx
@@ -16,50 +16,47 @@ const HeadlineWaiting: React.FC<Props> = () => {
       slg:w-[702]"
       >
         <div
-          className="
-            text-[black] whitespace-normal overflow-hidden font-normal text-center font-courierPrime leading-tight 
-            animate__animated animate__backInDown
-            mb-[50px] text-[14px] // Default for the smallest screens
-            xs:mb-[60px] xs:text-[20px] // Increased for mobile
-            xms:mb-[70px] xms:text-[22px] // Adjust if needed for slightly larger screens
-            md:mb-[90px] md:text-[28px]
-            lg:mb-[95px] lg:text-[38px]
-            xl:mb-[120px] xl:text-[48px]
-            "
+          className="text-[black] whitespace-normal overflow-hidden font-normal text-center font-courierPrime leading-tight 
+          animate__animated animate__backInDown
+        mb-[50px] text-[12px] 
+        xs:mb-[60px] xs:text-[15px] 
+        xms:mb-[70px] xms:text-[20px] 
+        md:mb-[90px] md:text-[30px] 
+        lg:mb-[95px] lg:text-[35px]
+        xl:mb-[120px] xl:text-[45px]
+        "
         >
-          Professional comedians may get 1 in 20 funny ideas...
+          Professional comedians may get 1 in 20 funny bits...
         </div>
 
-        <button
-          className="
-            rounded-full bg-gradient-to-r from-orange-100 to-orange-200 text-white font-copperplate leading-6
-            w-[133px] text-[16px] py-2 px-5
-            xs:w-[153px] xs:text-[20px] xs:py-2 xs:px-5
-            slg:w-[183px] slg:text-[23px] slg:py-3 slg:px-8
-            md:w-[200px] md:text-[26px] md:py-3 md:px-8  // Updated for medium devices
-            lg:w-[220px] lg:text-[30px] lg:py-4 lg:px-10 // Updated for large devices
-            xl:w-[240px] xl:text-[34px] xl:py-4 xl:px-12 // Updated for extra-large devices
+        <div className="animate__animated animate__tada">
+          <button
+            className="
+          rounded-full bg-gradient-to-r from-orange-100 to-orange-200 text-white font-copperplate leading-6
+          w-[133px] text-[16px] py-2 px-5
+          xs:w-[153px] xs:text-[20px] xs:py-2 xs:px-5
+          slg:w-[183px] slg:text-[23px] slg:py-3 slg:px-8 
           "
-        >
-          pffting
-          <span className="pl-1">
-            <PulseLoader color="#ffffff" size={2} />
-          </span>
-        </button>
-
+          >
+            pffting
+            <span className="pl-1">
+              <PulseLoader color="#ffffff" size={3} />
+            </span>
+          </button>
+        </div>
 
         <div
-          className=" text-[black] whitespace-normal overflow-hidden font-400 text-center font-courierPrime 
-            animate__animated animate__backInUp
-            mt-[40px] text-[20px] leading-tight // Default for the smallest screens, adjust leading if needed
-            xs:mt-[45px] xs:text-[30px] xs:leading-[40px] // Increased size and decreased line spacing for mobile
-            xms:mt-[50px] xms:text-[26px]
-            md:mt-[100px] md:text-[50px] md:leading-[73px]
-            lg:mt-[108px] lg:text-[50px]
-            xl:mt-[122px] xl:text-[50px]
-            "
+          className=" text-[black] whitespace-normal overflow-hidden font-400 text-center font-courierPrime leading-[73px] 
+          animate__animated animate__backInUp
+        mt-[40px] text-[20px]
+        xs:mt-[45px] xs:text-[24px]
+        xms:mt-[50px] xms:text-[26px]
+        md:mt-[100px] md:text-[50px]
+        lg:mt-[108px] lg:text-[50px]
+        xl:mt-[122px] xl:text-[50px]
+        "
         >
-          we average 1 in 10. 
+          we average 1 in 10
         </div>
       </div>
     </div>
