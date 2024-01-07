3489e3ba7ee2b329d6ce7197a6079a4b5d2086c8
aiartstyle50 <112304615+aiartstyle50@users.noreply.github.com>
2024-01-07 10:06:04 -0500

    Update main.py - old model & longer processing

--- aiservice/main.py
+++ aiservice/main.py
@@ -40,7 +40,7 @@ def contains_blocked_words(text: str, blocked_words_list: List[str]) -> bool:
             return True
     return False
 
-def generate_text(prompt: str, engine="ft:davinci-002:ai100::8ciHX6XM", max_tokens: int = 274, stop: Optional[str] = None, temperature: float = 0.7) -> str:
+def generate_text(prompt: str, engine="davinci:ft-ai100-2023-06-03-18-54-09", max_tokens: int = 124, stop: Optional[str] = None, temperature: float = 0.8) -> str:
     full_prompt = "" + prompt + "->"
     print(f"Sending prompt to OpenAI: {full_prompt}")
     response = openai.Completion.create(
@@ -58,17 +58,17 @@ def generate_text(prompt: str, engine="ft:davinci-002:ai100::8ciHX6XM", max_toke
 def process_opinion(opinion: str, processing_count: int) -> str:
     mod_value = processing_count % 7
     if mod_value == 0:
-        prompt = "<s>[INST] Add very specific, comedically hyperbolic detail to hyperbolically justify the opinion. Output one detailed sentence, then add one space and ###. OPINION: " + opinion + "\nOUTPUT: [/INST]"
+        prompt = "<s>[INST] Add very specific, comedically hyperbolic detail to hyperbolically justify the opinion. Output one long and detailed sentence, then add one space and ###. OPINION: " + opinion + "\nOUTPUT: [/INST]"
     elif mod_value == 1:
-        prompt = "<s>[INST] Add very specific hyperbolic detail to foolishly deny the opinion. Output one detailed sentence, then add one space and ###. OPINION: " + opinion + "\nOUTPUT: [/INST]"
+        prompt = "<s>[INST] Add very specific hyperbolic detail to foolishly deny the opinion. Output one long and detailed sentence, then add one space and ###. OPINION: " + opinion + "\nOUTPUT: [/INST]"
     elif mod_value == 2:
         prompt = "<s>[INST] INSTRUCTIONS: take the opposite of the opinion and justify it hyperbolically ironically with a specific detail or two in one detailed sentence, then add one space and ###. OPINION: " + opinion + "\nOUTPUT: [/INST]"
     elif mod_value == 3:
         prompt = "<s>[INST] INSTRUCTIONS: take the opposite of the opinion and justify it hyperbolically ironically with a specific detail or two in one detailed sentence, then add one space ###. OPINION: " + opinion + "\nOUTPUT: [/INST]"
     elif mod_value == 4:
-        prompt = "<s>[INST] Take the opinion and make it rationally justified with a specific persuasive and funny example. Output one detailed sentence, then add one space and ###. OPINION: " + opinion + "\nOUTPUT: [/INST]"
+        prompt = "<s>[INST] Take the opinion and make it rationally justified with a specific persuasive and funny example. Output one long detailed sentence, then add one space and ###. OPINION: " + opinion + "\nOUTPUT: [/INST]"
     elif mod_value == 5:
-        prompt = "<s>[INST] Repeat the opinion with more detail. Output one detailed sentence with no punctuation, then add one space and ###. OPINION: " + opinion + "\nOUTPUT: [/INST]"
+        prompt = "<s>[INST] Repeat the opinion with more detail. Output one detailed sentence, then add one space and ###. OPINION: " + opinion + "\nOUTPUT: [/INST]"
     elif mod_value == 6:
         prompt = "<s>[INST] Add extreme detail to the opinion and include no punctuation. Output one detailed sentence, then add one space and ###. OPINION: " + opinion + "\nOUTPUT: [/INST]"
 
@@ -76,7 +76,7 @@ def process_opinion(opinion: str, processing_count: int) -> str:
     payload = {
         "model": "mistralai/Mistral-7B-Instruct-v0.1",
         "prompt": prompt,
-        "max_tokens": 110,
+        "max_tokens": 140,
         "stop": ["##","[/INST]","</s>","###","#"],
         "temperature": 0.9,
         "top_p": 0.7,
@@ -120,15 +120,15 @@ def process_opinion(opinion: str, processing_count: int) -> str:
         return "", {"error": str(err)}  # Returns an empty string and the error information
 
 
-def check_and_retry(prompt: str, engine="ft:davinci-002:ai100::8ciHX6XM") -> str:
-    output = generate_text(prompt, engine=engine, max_tokens=100, stop=["##","!","<","#"])
+def check_and_retry(prompt: str, engine="davinci:ft-ai100-2023-06-03-18-54-09") -> str:
+    output = generate_text(prompt, engine=engine, max_tokens=120, stop=["##","!","<","#"])
     output = trim_text(output)  # Trim the output text
     plagiarism_results = check_plagiarism([output], spreadsheet_data)
     
     if not plagiarism_results:
         return output
     else:
-        output = generate_text(prompt, engine=engine, max_tokens=100, stop=["##","!","<","#"])
+        output = generate_text(prompt, engine=engine, max_tokens=120, stop=["##","!","<","#"])
         output = trim_text(output)  # Trim the output text again
         plagiarism_results = check_plagiarism([output], spreadsheet_data)
         
@@ -220,7 +220,7 @@ def generate_headline():
             processed_opinion, _ = process_opinion(opinion, i)  # Unpack the tuple to get the string
             processed_opinion = processed_opinion.lower()  # Now it's clear that processed_opinion is a string
             prompt = f"{processed_opinion} ->"
-            result = check_and_retry(prompt, engine="ft:davinci-002:ai100::8ciHX6XM")
+            result = check_and_retry(prompt, engine="davinci:ft-ai100-2023-06-03-18-54-09")
             print(f"Received headline: {result}")  # Print statement after receiving the response
 
             if result:
