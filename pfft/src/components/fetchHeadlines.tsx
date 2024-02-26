93deb568d6f78868e47831d852684869eee344de
aiartstyle50 <112304615+aiartstyle50@users.noreply.github.com>
2024-02-26 17:05:52 -0500

    change button placement mobile headlines

--- pfft/src/components/fetchHeadlines.tsx
+++ pfft/src/components/fetchHeadlines.tsx
@@ -129,81 +129,60 @@ const FetchHeadlines: FC<HeadlinesProps> = ({
         <NoHeadline />
       ) : (
         <div className="overflow-x-hidden grid grid-cols-1 gap-x-1 mx-3 lg:mt-8 font-copperplate lg:grid-cols-6 justify-end h-screen">
+          {/* Headlines1 Mapping */}
           <div className="text-[black] lg:order-1 ml-2 col-span-2 lg:px-0 px-8">
             <ToastContainer />
-            {headlines1.map((headline, index) => {
-              //const mobileFontSize = index === 0 ? 24 : 16; // smaller sizes for mobile
-              const fontWeight = index === 0 ? 600 : 400;
-              return (
-                <div
-                  onClick={() => {
-                    handleOnClick(headline);
-                  }}
-                  style={{
-                    fontSize: `${isMobile() ? (index === 0 ? 24 : 16) : (index === 0 ? 36 : 22)}px`,
-                    fontWeight: `${fontWeight}`,
-                    animationDuration: "1s",
-                    animationDelay: "1s" // Adjust the delay as needed
-                  }}
-                  key={index + headline}
-                >
-                  <div className="cursor-pointer hover:bg-gradient-to-r from-orange-100 to-orange-200 rounded-2xl p-2 text-[black] animate__animated animate__backInLeft">
-                    {headline}
-                  </div>
-                  <hr className="h-px my-8 border-0 bg-[#E6E6E6]" />
-                </div>
-              );
-            })}
+            {headlines1.map((headline, index) => (
+              <div
+                onClick={() => handleOnClick(headline)}
+                style={{
+                  fontSize: `${isMobile() ? (index === 0 ? 24 : 16) : (index === 0 ? 36 : 22)}px`,
+                  fontWeight: `${index === 0 ? 600 : 400}`,
+                  animationDuration: "1s",
+                  animationDelay: `${index * 0.5}s`,
+                }}
+                key={index + headline}
+                className="cursor-pointer hover:bg-gradient-to-r from-orange-100 to-orange-200 rounded-2xl p-2 text-[black] animate__animated animate__backInLeft"
+              >
+                {headline}
+                <hr className="h-px my-8 border-0 bg-[#E6E6E6]" />
+              </div>
+            ))}
           </div>
-
-          <div
-            className="order-first flex flex-col items-center justify-center p-4 col-span-2
-          text-[20px]
-          xms:text-[22px]
-          lg:text-[22px]
-          lg:order-2"
-          >
-            <div
-              className="text-center font-courierPrime mb-4
-           "
-            >
-              Choose your pffts
+          {/* Headlines2 Mapping */}
+          <div className="text-[black] lg:text-right lg:order-3 col-span-2 mx-3 lg:px-0 lg:ml-0 ml-2 px-8">
+            {headlines2.map((headline, index) => (
+              <div
+                onClick={() => handleOnClick(headline)}
+                style={{
+                  fontSize: `${isMobile() ? (index === 0 ? 24 : 16) : (index === 0 ? 36 : 22)}px`,
+                  fontWeight: `${index === 0 ? 600 : 400}`,
+                  animationDuration: "1s",
+                  animationDelay: `${index * 0.5}s`,
+                }}
+                key={index + headline}
+                className="cursor-pointer hover:bg-gradient-to-r from-orange-100 to-orange-200 rounded-2xl p-2 text-[black] animate__animated animate__backInRight"
+              >
+                {headline}
+                <hr className="h-px my-8 border-0 bg-[#E6E6E6]" />
+              </div>
+            ))}
+          </div>
+          {/* Conditional Rendering for Mobile */}
+          {isMobile() && (
+            <div className="flex flex-col items-center justify-center p-4 col-span-2 text-[20px] xms:text-[22px] lg:text-[22px]">
+              <div className="text-center font-courierPrime mb-4">
+                Choose your pffts
+              </div>
+              <div className="text-center font-courierPrime mb-4">or</div>
+              <button
+                onClick={handleRePFFT}
+                className="mt-0 mb-6 rounded-3xl bg-gradient-to-r from-orange-100 to-orange-200 w-[13rem] h-[3.8rem] font-courierPrime font-bold text-white"
+              >
+                re-pfft
+              </button>
             </div>
-            <div className="text-center font-courierPrime mb-4">or</div>
-            <button
-              onClick={() => {
-                handleRePFFT();
-              }}
-              className="lg:mt-5 mt-0 mb-6 lg:mb-0 rounded-3xl bg-gradient-to-r from-orange-100 to-orange-200 w-[13rem] h-[3.8rem] font-courierPrime font-bold text-white"
-            >
-              re-pfft
-            </button>
-          </div>
-
-          <div className="text-[black] lg:text-right lg:order-3 col-span-2 mx-3 lg:px-0 lg:ml-0 ml-2 px-8  ">
-            {headlines2.map((headline, index) => {
-              return (
-                <div
-                  onClick={() => {
-                    handleOnClick(headline);
-                  }}
-                  style={{
-                    fontSize: `${isMobile() ? (index === 0 ? 24 : 16) : (index === 0 ? 36 : 22)}px`,
-                    fontWeight: `${index === 0 ? 600 : 400}`,
-                    animationDuration: "1s",
-                    animationDelay: "1s"
-                  }}
-                  key={index + headline}
-                >
-                  <div className="cursor-pointer hover:bg-gradient-to-r from-orange-100 to-orange-200 rounded-2xl p-2 text-[black] animate__animated animate__backInRight">
-                    {headline}
-                  </div>
-                  <hr className="h-px my-8 border-0 bg-[#E6E6E6]" />
-                </div>
-              );
-            })}
-
-          </div>
+          )}
         </div>
       )}
     </>
