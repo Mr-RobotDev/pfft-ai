45da4e92444d1f9b0b174d91a6c6fe7127badefc
Abdullah Shahid <abdullahshahid1071@gmail.com>
2024-03-12 21:53:33 +0500

    Add cookies consent pop to app

--- pfft/src/pages/_app.tsx
+++ pfft/src/pages/_app.tsx
@@ -1,11 +1,13 @@
-'use client'
+"use client";
 import "../styles/global.css";
 
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import CookiesConsent from "@/components/CookiesConsent";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <SessionProvider session={pageProps.session}>
+    <CookiesConsent />
     <Component {...pageProps} />
   </SessionProvider>
 );
